e88f047eb03684492b1d93416bb386b63bc50991
Miguel Gonzalez <47222395+miguelgh65@users.noreply.github.com>
2023-08-03 13:06:09 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,37 @@
-function updateClock() {
-    const now = new Date();
-    const hours = String(now.getHours()).padStart(2, '0');
-    const minutes = String(now.getMinutes()).padStart(2, '0');
-    const seconds = String(now.getSeconds()).padStart(2, '0');
-    document.getElementById('clock').textContent = `${hours}:${minutes}:${seconds}`;
-}
-setInterval(updateClock, 1000);
-updateClock(); 
+ function updateTime() {
+  var dateInfo = new Date();
+ 
+
+ /* time */
+  var hr,
+    _min = (dateInfo.getMinutes() < 10) ? “0” + dateInfo.getMinutes() : dateInfo.getMinutes(),
+    sec = (dateInfo.getSeconds() < 10) ? “0” + dateInfo.getSeconds() : dateInfo.getSeconds(),
+    ampm = (dateInfo.getHours() >= 12) ? “PM” : “AM”;
+  
+
+// replace 0 with 12 at midnight, subtract 12 from hour if 13–23
+  if (dateInfo.getHours() == 0) {
+    hr = 12;
+  } else if (dateInfo.getHours() > 12) {
+    hr = dateInfo.getHours() – 12;
+  } else {
+    hr = dateInfo.getHours();
+  }
+
+  var currentTime = hr + “:” + _min + “:” + sec;
+  
+
+// print time
+  document.getElementsByClassName(“hms”)[0].innerHTML = currentTime;
+  document.getElementsByClassName(“ampm”)[0].innerHTML = ampm;
+
+  
+// Add date & Month (javascript)
+
+};
+
+// print time and date once, then update them every second
+updateTime();
+setInterval(function() {
+  updateTime()
+}, 1000);
